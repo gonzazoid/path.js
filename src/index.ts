c7abd812759b1af9ffd57b8b12a541fba8416f37
Timur Appaev <alloaolla@gmail.com>
2017-04-20 23:13:20 +0300

    webpack removed

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 export const Path = function<T>(constr: T, path?: string): T{
 
-    const target: String = new (String as any)(path);
+    const target: String = new (String as any)(path ? path : '');
 
     return new Proxy(target, {
-      get(target: any, prop: string) {
-        return prop in target || typeof prop !== 'string' ? target[prop] :  Path(path ? `${path}.${prop}` : prop);
+      get(target: any, prop: any) {
+        return (constr as any)[prop] ? Path((constr as any)[prop], path ? `${path}.${prop}` : prop) : target[prop];
       },
       set(target: any, prop: string, value: any) {
         return false;
